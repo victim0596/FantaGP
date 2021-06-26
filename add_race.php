ce54e3b4ab4b0b5c011d21e9fd5ab7567708cde7
victim0596 <81644467+victim0596@users.noreply.github.com>
2021-06-26 09:30:22 +0200

    sistemato lato backend

--- add_race.php
+++ add_race.php
@@ -1,42 +1,39 @@
 <?php
-if (isset($_SESSION['session_id'])){
-	if(isset($_POST['add_res_race'])){
-	$id_p= $_SESSION['session_user'];
-	$nome_gara=filter_input(INPUT_POST, 'nome_gara');
-	$gp1=filter_input(INPUT_POST, 'gp1');
-	$gp2=filter_input(INPUT_POST, 'gp2');
-	$gp3=filter_input(INPUT_POST, 'gp3');
-	$giro_veloce=filter_input(INPUT_POST, 'giro_veloce');
-	$n_ritirati=filter_input(INPUT_POST, 'n_ritirati');
-	$VSC=filter_input(INPUT_POST, 'vsc');
-	$SC=filter_input(INPUT_POST, 'sc');
-	if(!empty($id_p) and !empty($nome_gara) and !empty($gp1) and !empty($gp2) and !empty($gp3) and !empty($giro_veloce) and $n_ritirati>=0 and !empty($VSC) and !empty($SC)){
-
-		include 'connection.php';
-		if(mysqli_connect_error()){
-			die('Errore di connessione ('.mysqli_connect_error().')');
-		}
-		else {
-		//	$link=mysql_connect($host, $dbusername, $dbpassword);  php 5
-		//	mysql_select_db($dbname,$link);   php 5
-				$sql="UPDATE risultati set gp1='$gp1',gp2='$gp2',gp3='$gp3', giro_veloce='$giro_veloce', n_ritirati='$n_ritirati', vsc='$VSC', sc='$SC' where nome_gara='$nome_gara'";
-					if($conn->query($sql)){
-						$text= "I dati sono stati inseriti correttamente";
-					}
-					else{
-						$text= "Error: ".$sql."<br>".$conn->error;
-					}
-					
-		}
-		$conn->close();
-	}
-	else{
-		$text= "Non hai messo tutti i dati";
-	}
-  }
+if (isset($_SESSION['session_id'])) {
+    if (isset($_POST['add_res_race'])) {
+        $nome_gara = filter_input(INPUT_POST, 'nome_gara');
+        $gp1 = filter_input(INPUT_POST, 'gp1');
+        $gp2 = filter_input(INPUT_POST, 'gp2');
+        $gp3 = filter_input(INPUT_POST, 'gp3');
+        $giro_veloce = filter_input(INPUT_POST, 'giro_veloce');
+        $n_ritirati = filter_input(INPUT_POST, 'n_ritirati');
+        $VSC = filter_input(INPUT_POST, 'vsc');
+        $SC = filter_input(INPUT_POST, 'sc');
+        if (!empty($nome_gara) and !empty($gp1) and !empty($gp2) and !empty($gp3) and !empty($giro_veloce) and $n_ritirati >= 0 and !empty($VSC) and !empty($SC)) {
+            include 'connection.php';
+            if (mysqli_connect_error()) {
+                die('Errore di connessione (' . mysqli_connect_error() . ')');
+            } else {
+                //    $link=mysql_connect($host, $dbusername, $dbpassword);  php 5
+                //    mysql_select_db($dbname,$link);   php 5
+                $sql = "SELECT * from risultati where nome_gara='$nome_gara' and gp1 is not null";
+                $result = $conn->query($sql);
+                $num_row = $result->num_rows;
+                if ($num_row == 0) {
+                    $sql = "UPDATE risultati set gp1='$gp1',gp2='$gp2',gp3='$gp3', giro_veloce='$giro_veloce', n_ritirati='$n_ritirati', vsc='$VSC', sc='$SC' where nome_gara='$nome_gara'";
+                    if ($conn->query($sql)) {
+                        $text = "I dati sono stati inseriti correttamente";
+                    } else {
+                        $text = "Error: " . $sql . "<br>" . $conn->error;
+                    }
+                }
+				$text = "Hai giá inserito i risultati della gara";
+            }
+            $conn->close();
+        } else {
+            $text = "Non hai messo tutti i dati";
+        }
+    }
+} else {
+    $text = "Effettua prima il login";
 }
-else{
-	$text= "Effettua prima il login";
-}
-
-?>
