b1231f62741ddee9f32cf6a6d9fa400f0d07b381
victim0596 <81644467+victim0596@users.noreply.github.com>
2021-06-30 19:49:29 +0200

    Aggiornata versione

--- js/pronoValidation.js
+++ js/pronoValidation.js
@@ -1,17 +1,17 @@
 var driverArray = ["Hamilton", "Bottas", "Verstappen", "Perez", "Sainz", "Leclerc", "Ricciardo", "Norris", "Alonso", "Ocon",
-"Giovinazzi", "Raikkonen", "Gasly", "Tsunoda", "Russel", "Latifi", "Mazepin", "Schumacher", "Vettel", "Stroll"];
+    "Giovinazzi", "Raikkonen", "Gasly", "Tsunoda", "Russel", "Latifi", "Mazepin", "Schumacher", "Vettel", "Stroll"];
 
 var pronoQualy = {
-    qp1 : "",
-    qp2 : "",
-    qp3 : ""
+    qp1: "",
+    qp2: "",
+    qp3: ""
 }
 
 var pronoRace = {
-    gp1 : "",
-    gp2 : "",
-    gp3 : "",
-    fastLap : "",
+    gp1: "",
+    gp2: "",
+    gp3: "",
+    fastLap: "",
 }
 
 var qp1Form = document.getElementsByName("qp1")[0];
@@ -29,84 +29,84 @@ var btnRace = document.getElementsByName("invia_race")[0];
 var urlPage = window.location.href;
 
 
-btnQualy.disabled = true;  
+btnQualy.disabled = true;
 btnRace.disabled = true;
 
 
 /* FORM QUALIFICA */
-qp1Form.addEventListener('input', function(){
+qp1Form.addEventListener('input', function () {
     pronoQualy.qp1 = qp1Form.value;
     checkDriverQualy();
 });
-qp2Form.addEventListener('input', function(){
+qp2Form.addEventListener('input', function () {
     pronoQualy.qp2 = qp2Form.value;
     checkDriverQualy();
 });
-qp3Form.addEventListener('input', function(){
+qp3Form.addEventListener('input', function () {
     pronoQualy.qp3 = qp3Form.value;
     checkDriverQualy();
 });
 
 
 /* FORM GARA */
-gp1Form.addEventListener('input', function(){
+gp1Form.addEventListener('input', function () {
     pronoRace.gp1 = gp1Form.value;
     checkDriverRace();
 });
-gp2Form.addEventListener('input', function(){
+gp2Form.addEventListener('input', function () {
     pronoRace.gp2 = gp2Form.value;
     checkDriverRace();
 });
-gp3Form.addEventListener('input', function(){
+gp3Form.addEventListener('input', function () {
     pronoRace.gp3 = gp3Form.value;
     checkDriverRace();
 });
 
-fastLapForm.addEventListener('input', function(){
+fastLapForm.addEventListener('input', function () {
     pronoRace.fastLap = fastLapForm.value;
     checkDriverRace();
 });
 
 
+//ricordati di cambiare i link
+function checkDriverQualy() {
+    if (pronoQualy.qp1 == pronoQualy.qp2 || pronoQualy.qp1 == pronoQualy.qp3 || pronoQualy.qp2 == pronoQualy.qp3) {
+        if (urlPage == "http://localhost/Git/FantaGP/pronostici.php") textError.innerHTML = "Hai inserito uno stesso pilota in uno degli altri campi";
+        else textErrorProfile.innerHTML = "Hai inserito uno stesso pilota in uno degli altri campi";
 
-function checkDriverQualy(){
-    if(pronoQualy.qp1 == pronoQualy.qp2 || pronoQualy.qp1 == pronoQualy.qp3 || pronoQualy.qp2 == pronoQualy.qp3){
-        if(urlPage == "http://fantagp2021.cu.ma/pronostici.php") textError.innerHTML = "Hai inserito uno stesso pilota in uno degli altri campi";
-        else  textErrorProfile.innerHTML = "Hai inserito uno stesso pilota in uno degli altri campi";
-
-        }
+    }
     else {
-        if(driverArray.indexOf(pronoQualy.qp1)>=0 && driverArray.indexOf(pronoQualy.qp2)>=0 && driverArray.indexOf(pronoQualy.qp3)>=0){
+        if (driverArray.indexOf(pronoQualy.qp1) >= 0 && driverArray.indexOf(pronoQualy.qp2) >= 0 && driverArray.indexOf(pronoQualy.qp3) >= 0) {
             btnQualy.disabled = false;
-            if(urlPage == "http://fantagp2021.cu.ma/pronostici.php") textError.innerHTML = "";
+            if (urlPage == "http://localhost/Git/FantaGP/pronostici.php") textError.innerHTML = "";
             else textErrorProfile.innerHTML = "";
         }
         else {
-            if(urlPage == "http://fantagp2021.cu.ma/pronostici.php") textError.innerHTML = "Hai inserito un pilota non esistente/Campo vuoto";
+            if (urlPage == "http://localhost/Git/FantaGP/pronostici.php") textError.innerHTML = "Hai inserito un pilota non esistente/Campo vuoto";
             else textErrorProfile.innerHTML = "Hai inserito un pilota non esistente/Campo vuoto";
-            btnQualy.disabled = true;  
+            btnQualy.disabled = true;
         }
     }
 }
 
 
-
-function checkDriverRace(){
-    if(pronoRace.gp1 == pronoRace.gp2 || pronoRace.gp1 == pronoRace.gp3 || pronoRace.gp2 == pronoRace.gp3){
-        if(urlPage == "http://fantagp2021.cu.ma/pronostici.php") textError.innerHTML = "Hai inserito uno stesso pilota in uno degli altri campi";
+//ricordati di cambiare i link
+function checkDriverRace() {
+    if (pronoRace.gp1 == pronoRace.gp2 || pronoRace.gp1 == pronoRace.gp3 || pronoRace.gp2 == pronoRace.gp3) {
+        if (urlPage == "http://localhost/Git/FantaGP/pronostici.php") textError.innerHTML = "Hai inserito uno stesso pilota in uno degli altri campi";
         else textErrorProfile.innerHTML = "Hai inserito uno stesso pilota in uno degli altri campi";
     }
     else {
         //controllo se i piloti del form gara sono esistenti
-        if(driverArray.indexOf(pronoRace.gp1)>=0 && driverArray.indexOf(pronoRace.gp2)>=0 && driverArray.indexOf(pronoRace.gp3)>=0 && driverArray.indexOf(pronoRace.fastLap)>=0){
+        if (driverArray.indexOf(pronoRace.gp1) >= 0 && driverArray.indexOf(pronoRace.gp2) >= 0 && driverArray.indexOf(pronoRace.gp3) >= 0 && driverArray.indexOf(pronoRace.fastLap) >= 0) {
             btnRace.disabled = false;
-            if(urlPage == "http://fantagp2021.cu.ma/pronostici.php") textError.innerHTML = "";
-            else textErrorProfile.innerHTML = ""; 
+            if (urlPage == "http://localhost/Git/FantaGP/pronostici.php") textError.innerHTML = "";
+            else textErrorProfile.innerHTML = "";
         }
         else {
-            if(urlPage == "http://fantagp2021.cu.ma/pronostici.php") textError.innerHTML = "Hai inserito un pilota non esistente/Campo vuoto";
+            if (urlPage == "http://localhost/Git/FantaGP/pronostici.php") textError.innerHTML = "Hai inserito un pilota non esistente/Campo vuoto";
             else textErrorProfile.innerHTML = "Hai inserito un pilota non esistente/Campo vuoto";
-            btnRace.disabled = true;  
+            btnRace.disabled = true;
         }
     }
 }
