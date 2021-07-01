6d25313cf08ba8cf6b3b5b7db7a470327e175c3b
victim0596 <81644467+victim0596@users.noreply.github.com>
2021-07-01 15:54:13 +0200

    Aggiunti controlli di sicurezza, e sha256 per la psw

--- login2.php
+++ login2.php
@@ -6,30 +6,36 @@ if (isset($_SESSION['session_id'])) {
     exit;
 }
 if (isset($_POST['login'])) {
-    $n_ut = filter_input(INPUT_POST, 'n_utente');
+    $n_ut = $_POST['n_utente'];
     $n_psw = filter_input(INPUT_POST, 'psw');
     if (!empty($n_ut) && !empty($n_psw)) {
         include 'connection.php';
         if (mysqli_connect_error()) {
             die('Errore di connessione (' . mysqli_connect_error() . ')');
         } else {
-            //$link=mysql_connect($host, $dbusername, $dbpassword); php 5
-            //mysql_select_db($dbname,$link); php 5
-            $sql = "SELECT * from utenti where username='$n_ut' and password='$n_psw'";
-            $result = $conn->query($sql);
-            $num_row = $result->num_rows;
+            $sql = "SELECT username,password from utenti where username= ? ";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("s", $n_ut);
+            $stmt->execute();
+            $stmt->store_result();
+            $num_row = $stmt->num_rows;
             if ($num_row == 1) {
-                session_regenerate_id();
-                $_SESSION['session_id'] = session_id();
-                $_SESSION['session_user'] = $n_ut;
-                header('Location: index.php');
-                $conn->close();
-                exit;
+                $stmt->bind_result($id, $password);
+	            $stmt->fetch();
+                $salt = "0x618f0554f66153b508be1813c76c26bb";
+                $psw_salted = hash_hmac("sha256", $n_psw, $salt);
+                if(password_verify($psw_salted,$password)){
+                    session_regenerate_id();
+                    $_SESSION['session_id'] = session_id();
+                    $_SESSION['session_user'] = $n_ut;
+                    header('Location: index.php');
+                    exit;
+                } else echo "Hai inserito una password sbagliata";
             } else {
                 echo "Hai inserito dati errati";
-                $conn->close();
             }
         }
+        $stmt->close();
         $conn->close();
     } else {
         echo "Non hai messo tutti i dati";
