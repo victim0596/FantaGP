42117af88f0cb1ebf7882647c0641c99b31189a5
victim0596 <81644467+victim0596@users.noreply.github.com>
2021-07-18 18:34:57 +0200

    Aggiunto controllo su nome utente
    tolti possibili spazi vuoti

--- login2.php
+++ login2.php
@@ -5,7 +5,8 @@ if (isset($_SESSION['session_id'])) {
     exit;
 }
 if (isset($_POST['login'])) {
-    $n_ut = $_POST['n_utente'];
+    $n_ut_input = $_POST['n_utente'];
+    $n_ut = str_replace(" ", "", $n_ut_input);
     $n_psw = filter_input(INPUT_POST, 'psw');
     if (!empty($n_ut) && !empty($n_psw)) {
         include 'connection.php';
@@ -20,16 +21,19 @@ if (isset($_POST['login'])) {
             $num_row = $stmt->num_rows;
             if ($num_row == 1) {
                 $stmt->bind_result($id, $password);
-	            $stmt->fetch();
+                $stmt->fetch();
                 $salt = "0x618f0554f66153b508be1813c76c26bb";
                 $psw_salted = hash_hmac("sha256", $n_psw, $salt);
-                if(password_verify($psw_salted,$password)){
+                if (password_verify($psw_salted, $password)) {
                     session_start();
                     $_SESSION['session_id'] = session_id();
                     $_SESSION['session_user'] = $n_ut;
                     header('Location: profilo.php');
                     exit;
-                } else echo "Hai inserito una password sbagliata";
+                } else {
+                    echo "Hai inserito una password sbagliata";
+                }
+
             } else {
                 echo "Hai inserito dati errati";
             }
