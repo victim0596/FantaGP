4faa17fd418bf9b5a95b53b5b1cce708617816ae
victim0596 <81644467+victim0596@users.noreply.github.com>
2021-10-29 09:15:39 +0200

    Àggiunti alert alla pagina

--- profilo.php
+++ profilo.php
@@ -20,10 +20,10 @@ if (!isset($_SESSION['session_id'])) {
   <title>FantaGP 2021</title>
   <meta name="description" content="Sito per il fantaGP">
   <meta name="author" content="Oliver Terzo">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/main.css?n=1.01">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/main.css?n=1.03">
   <link rel="stylesheet" href="css/pronostici.css?n=1.01">
-  <link rel="stylesheet" href="css/profilo.css?n=1.01">
+  <link rel="stylesheet" href="css/profilo.css?n=1.03">
   <meta name="viewport" content="initial-scale=1, width=device-width, maximum-scale=1, user-scalable=no, shrink-to-fit=no">
   <link rel="shortcut icon" href="/logo.ico" />
 </head>
@@ -158,11 +158,9 @@ if (!isset($_SESSION['session_id'])) {
   </div>
 
 
-  <?php include 'riepilogo_pronostici.php'; ?>
-  <?php include 'mod_quali.php'; ?>
-  <?php include 'mod_race.php'; ?>
-  <div class="text_modpron"><?php echo $text; ?></div>
-
+  <?php include 'riepilogo_pronostici.php';
+  include 'mod_quali.php';
+  include 'mod_race.php'; ?>
 
 
   <!-- Modal Riepilogo pronostici -->
@@ -171,7 +169,7 @@ if (!isset($_SESSION['session_id'])) {
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="modalpron">Riepilogo Pronostici</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
 
           </button>
         </div>
@@ -198,7 +196,7 @@ if (!isset($_SESSION['session_id'])) {
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="modalqualy">Modifica Pronostici Qualifica</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
           </button>
         </div>
         <div class="modal-body">
@@ -235,7 +233,7 @@ if (!isset($_SESSION['session_id'])) {
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="modalrace">Modifica Pronostici Gara</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
           </button>
         </div>
         <div class="modal-body">
@@ -341,22 +339,21 @@ if (!isset($_SESSION['session_id'])) {
     //Sessione Admin
 
     if ($_SESSION['session_user'] == "Oliver") {
+      require('./script/addRitiratiScript.php');
       include 'calcolo_punteggi.php';
-      include 'add_rit.php';
-      include 'add_qualy.php';
-      include 'add_race.php';
-      include 'add_pagelle.php';
+      require('./script/addQualyScript.php');
+      require('./script/addRaceScript.php');
+      require('./script/addPagelleScript.php');
   ?>
 
-      <div class="position-absolute text"><?php echo $text; ?></div>
-      <div class="position-absolute text_addrit"><?php echo $text1; ?></div>
+      </div>
       <!-- Modal Aggiungi risultati Qualifica -->
       <div class="modal fade" id="modaladdq" tabindex="-1" aria-labelledby="modaladdq" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-sm">
           <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title" id="modaladdq">Aggiungi risultati Qualifica</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
               </button>
             </div>
             <div class="modal-body">
@@ -394,7 +391,7 @@ if (!isset($_SESSION['session_id'])) {
           <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title" id="modaladdr">Aggiungi risultati Gara</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
               </button>
             </div>
             <div class="modal-body">
@@ -448,7 +445,7 @@ if (!isset($_SESSION['session_id'])) {
           <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title" id="modaladdp">Aggiungi Pagelle</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
               </button>
             </div>
             <div class="modal-body">
@@ -490,7 +487,7 @@ if (!isset($_SESSION['session_id'])) {
           <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title" id="modaladdrit">Aggiungi Ritirato</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
               </button>
             </div>
             <div class="modal-body">
@@ -524,7 +521,7 @@ if (!isset($_SESSION['session_id'])) {
           <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title" id="modalcalcolo">Calcolo Punteggi</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
               </button>
             </div>
             <div class="modal-body">
@@ -549,7 +546,7 @@ if (!isset($_SESSION['session_id'])) {
           <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title" id="modalQuery">Inserimento Query</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
               </button>
             </div>
             <div class="modal-body">
@@ -574,12 +571,29 @@ if (!isset($_SESSION['session_id'])) {
 
   <?php }
   } ?>
+<!-- simboli messaggi -->
+<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+  <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+  </symbol>
+  <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+  </symbol>
+</svg>
+<!-- Alert -->
 
+  <div class="alert alert-primary d-flex align-items-center classAlert" id="alert" role="alert">
+    <div class="textProfile" id="txtAlert">
+    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Info:"><use xlink:href="#check-circle-fill" id="simbolID"/></svg>  
+    <?php echo $text; ?></div>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" id="btnAlert"></button>
+  </div>
 
-
+  <script src="js/Alert.js"></script>
   <script src="js/profilo.js"></script>
   <script src="js/pronoValidation.js"></script>
   <script src="js/actualRace.js?n=1.0"></script>
+  
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
