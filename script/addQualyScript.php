17f9a12f884accd40c16005968c88110034e4e29
victim0596 <81644467+victim0596@users.noreply.github.com>
2021-10-29 09:07:38 +0200

    Aggiornamento con classi
    Rimossi file vecchi, aggiunti con le varie classi e spostati nella cartella script

---
+++ script/addQualyScript.php
@@ -0,0 +1,23 @@
+<?php
+if (isset($_SESSION['session_id'])) {
+    if (isset($_POST['add_res_quali'])) {
+        $nome_gara = $_POST['nome_gara'];
+        $qp1 = $_POST['qp1'];
+        $qp2 = $_POST['qp2'];
+        $qp3 = $_POST['qp3'];
+        try{
+            $form = new FormCheck($nome_gara);
+            $checkAddQualy = $form->checkQualyForm($qp1, $qp2, $qp3);
+            if($checkAddQualy['boolValue']){
+                $qExec = new QExec();
+                $text = $qExec->insertResultQualy($nome_gara,$qp1,$qp2,$qp3);
+            }else{
+                $text = $checkAddQualy['error'];
+            }
+        }catch(Exception $ex){
+            $text = $ex->getMessage(); 
+        }
+    }
+} else {
+    $text = "Effettua prima il login";
+}
