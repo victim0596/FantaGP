0a11cbcf547b0035e2f5f0b742281ce05dcde59b
victim0596 <81644467+victim0596@users.noreply.github.com>
2021-10-29 09:10:58 +0200

    Aggiornamento con classi
    Rimosso file originale e aggiornato con le varie classi, spostato nella cartella script

--- script/loginScript.php
+++ script/loginScript.php
@@ -9,15 +9,22 @@ if (isset($_SESSION['session_id'])) {
 }
 if (isset($_POST['login'])) {
     $n_ut_input = $_POST['n_utente'];
-    $n_psw = filter_input(INPUT_POST, 'psw');
+    $n_psw = $_POST['psw'];
     $login = new LoginAuth($n_ut_input, $n_psw);
-    if ($login->loginCheckFormat()['boolValue']) {
-        if (!$login->checkDbAuth())  $text = "Dati errati";
-        else {
-            session_start();
-            $_SESSION['session_id'] = session_id();
-            $_SESSION['session_user'] = $n_ut_input;
-            header('Location: profilo.php');
-        }
-    } else  $text = $login->loginCheckFormat()['error'];
+    $loginCheck = $login->loginCheckFormat();
+    try{
+        if ($loginCheck['boolValue']) {
+            $queryEx = new QExec();
+            $loginAuth = $queryEx->checkDbAuth($n_ut_input, $n_psw);
+            if (!$loginAuth)  $text = "Dati errati";
+            else {
+                session_start();
+                $_SESSION['session_id'] = session_id();
+                $_SESSION['session_user'] = $n_ut_input;
+                header('Location: profilo.php');
+            }
+        } else  $text = $loginCheck['error'];
+    }catch(Exception $ex){
+        $text = $ex->getMessage();
+    }  
 }
