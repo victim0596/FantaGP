95ccadbd58b8311329e2a158c2d54513d0d6569e
victim0596 <81644467+victim0596@users.noreply.github.com>
2021-11-01 18:15:39 +0100

    Spostato e riscritto lo script per modificare i pronostici della qualifica

---
+++ script/modPronoQualy.php
@@ -0,0 +1,23 @@
+<?php
+
+if (isset($_SESSION['session_id'])) {
+    if (isset($_POST['invia_quali'])) {
+        $id_p = $_SESSION['session_user'];
+        $nome_gara = $_POST['nome_gara'];
+        $qp1 = $_POST['qp1'];
+        $qp2 = $_POST['qp2'];
+        $qp3 = $_POST['qp3'];
+        try{
+            $form = new FormCheck($nome_gara);
+            if(check_date_quali($nome_gara)) {
+                $checkQualy = $form->checkQualyForm($qp1, $qp2, $qp3);
+                if($checkQualy['boolValue']){
+                    $qExec = new QExec();
+                    $text = $qExec->modQualyProno($id_p, $nome_gara, $qp1, $qp2, $qp3);
+                }else $text = $checkQualy['error'];
+            } else $text = "Tempo limite superato";
+        }catch(Exception $ex){
+            $text = $ex->getMessage();
+        }
+    }
+}
