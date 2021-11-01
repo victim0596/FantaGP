b1903b3bdca8aa790f77a237836957c67d7c3230
victim0596 <81644467+victim0596@users.noreply.github.com>
2021-11-01 18:18:24 +0100

    Aggiunta funzione per trovare la gara corrente

--- script/time_limit.php
+++ script/time_limit.php
@@ -1,6 +1,6 @@
 <?php
 
-function check_date_quali($nome_gara):bool
+function check_date_quali($nome_gara): bool
 {
     $var_controllo = true;
     date_default_timezone_set("Europe/Rome");
@@ -156,4 +156,42 @@ function check_date_race($nome_gara)
     }
 
     return $var_controllo;
+}
+
+function CurrentRace(): string
+{
+    date_default_timezone_set("Europe/Rome");
+    $currentDate = date("Y-m-d H:i:s");
+    $race = "";
+    $raceList = array(
+        "Bahrein" => "2021-03-27 00:00:00",
+        "Italia-Imola" => "2021-04-17 00:00:00",
+        "Portogallo" => '2021-05-01 00:00:00',
+        "Spagna" => "2021-05-08 00:00:00",
+        "Monaco" => "2021-05-22 00:00:00",
+        "Azerbaigian" => "2021-06-05 00:00:00",
+        "Francia" => "2021-06-19 00:00:00",
+        "Austria" => "2021-06-26 00:00:00",
+        "Austria-2" => "2021-07-03 00:00:00",
+        "Gran Bretagna" => "2021-07-17 00:00:00",
+        "Ungheria" => "2021-07-31 00:00:00",
+        "Belgio" => "2021-08-28 00:00:00",
+        "Olanda" => "2021-09-04 00:00:00",
+        "Italia-Monza" => "2021-09-11 00:00:00",
+        "Russia" => "2021-09-25 00:00:00",
+        "Turchia" => "2021-10-09 00:00:00",
+        "USA" => "2021-10-23 00:00:00",
+        "Messico" => "2021-11-06 00:00:00",
+        "Brasile" => "2021-11-13 00:00:00",
+        "Qatar" => "2021-11-20 00:00:00",
+        "Arabia Saudita" => "2021-12-04 00:00:00",
+        "Emirati Arabi" => "2021-12-11 00:00:00"
+    );
+    foreach ($raceList as $raceName => $raceDate) {
+        if ($raceDate > $currentDate) {
+            break;
+        }
+        $race = $raceName;
+    }
+    return $race;
 }
