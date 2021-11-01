645af1df4bf8cf40fe841046f4280b5dfd0766e0
victim0596 <81644467+victim0596@users.noreply.github.com>
2021-11-01 18:17:52 +0100

    Spostati e organizzati tutti i require della pagina profilo

--- profilo.php
+++ profilo.php
@@ -9,6 +9,21 @@ if (!isset($_SESSION['session_id'])) {
   header('Location: index.php');
   exit;
 }
+$text = "";
+require('./classes/FormClass.php');
+require('./classes/QueryClass.php');
+include 'calcolo_punteggi.php';
+require('./script/time_limit.php');
+require('./script/addRitiratiScript.php');
+require('./script/addQualyScript.php');
+require('./script/addRaceScript.php');
+require('./script/addPagelleScript.php');
+require('./script/modPronoRace.php');
+require('./script/modPronoQualy.php');
+require('./script/loadPtProfile.php');
+require('./script/riepilogoPronostici.php');
+
+
 
 ?>
 
@@ -131,7 +146,6 @@ if (!isset($_SESSION['session_id'])) {
           <div class="divider" id="nomeSquadra"></div>
           <div class="punteggi divider">
             <div class="row">
-              <?php include 'profilo2.php'; ?>
               <div class="col p-3 text-center">
                 <div><?php echo $textPtTotali; ?></div>
                 <div>Punti totali</div>
@@ -158,9 +172,7 @@ if (!isset($_SESSION['session_id'])) {
   </div>
 
 
-  <?php include 'riepilogo_pronostici.php';
-  include 'mod_quali.php';
-  include 'mod_race.php'; ?>
+
 
 
   <!-- Modal Riepilogo pronostici -->
@@ -339,11 +351,7 @@ if (!isset($_SESSION['session_id'])) {
     //Sessione Admin
 
     if ($_SESSION['session_user'] == "Oliver") {
-      require('./script/addRitiratiScript.php');
-      include 'calcolo_punteggi.php';
-      require('./script/addQualyScript.php');
-      require('./script/addRaceScript.php');
-      require('./script/addPagelleScript.php');
+
   ?>
 
       </div>
@@ -571,21 +579,24 @@ if (!isset($_SESSION['session_id'])) {
 
   <?php }
   } ?>
-<!-- simboli messaggi -->
-<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-  <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
-    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-  </symbol>
-  <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
-    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-  </symbol>
-</svg>
-<!-- Alert -->
+  <!-- simboli messaggi -->
+  <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+    <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+    </symbol>
+    <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+      <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+    </symbol>
+  </svg>
+  <!-- Alert -->
 
   <div class="alert alert-primary d-flex align-items-center classAlert" id="alert" role="alert">
     <div class="textProfile" id="txtAlert">
-    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Info:"><use xlink:href="#check-circle-fill" id="simbolID"/></svg>  
-    <?php echo $text; ?></div>
+      <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Info:">
+        <use xlink:href="#check-circle-fill" id="simbolID" />
+      </svg>
+      <?php echo $text; ?>
+    </div>
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" id="btnAlert"></button>
   </div>
 
@@ -593,7 +604,7 @@ if (!isset($_SESSION['session_id'])) {
   <script src="js/profilo.js"></script>
   <script src="js/pronoValidation.js"></script>
   <script src="js/actualRace.js?n=1.0"></script>
-  
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
