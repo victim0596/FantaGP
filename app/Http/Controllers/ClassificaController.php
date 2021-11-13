645e787663502f8585a2acbc64fe782470e764c3
victim0596 <81644467+victim0596@users.noreply.github.com>
2021-11-13 02:54:11 +0100

    Update ClassificaController.php

--- app/Http/Controllers/ClassificaController.php
+++ app/Http/Controllers/ClassificaController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Classes\QExec;
-use App\Models\PronosticiModel;
 use Illuminate\Http\Request;
 
 class ClassificaController extends Controller
