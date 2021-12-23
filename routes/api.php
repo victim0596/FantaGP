94f232e37f8d16683365479dd010a85a2938923e
victim0596 <81644467+victim0596@users.noreply.github.com>
2021-12-23 13:14:20 +0100

    Aggiunte route api per la pagina statistiche

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\PronosticiModel;
+use App\Models\RisultatiModel;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,12 +21,27 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get('/prono/{utente}/{gara}', function(string $utente, string $gara){
+Route::get('/prono/{utente}/{gara}', function (string $utente, string $gara) {
     $date = PronosticiModel::where('id_p', $utente)->where('nome_gara', $gara)->first();
     return response()->json($date);
 });
 
-Route::get('/pronoAllClassifica/{utente}', function(string $utente){
+Route::get('/pronoAllClassifica/{utente}', function (string $utente) {
     $date = PronosticiModel::select('id_p', 'punti', 'punti_pron', 'punti_pag')->where('id_p', $utente)->get()->values()->all();
     return response()->json($date);
+});
+
+Route::get('/loadProno', function () {
+    $dataPronDB = PronosticiModel::whereRaw('punti IS NOT NULL')->orderBy('id_p', 'ASC')->orderBy('nome_gara', 'ASC')->get();
+    $dataPron = $dataPronDB->values()->all();
+    $dataPronLength = count($dataPron);
+    $num_row = $dataPronLength / 10;
+    $arrayGara = array_chunk($dataPron, $num_row);
+    return response()->json($arrayGara);
+});
+
+Route::get('/loadResult', function () {
+    $dataResultDB = RisultatiModel::all();
+    $dataResul = $dataResultDB->values()->all();
+    return response()->json($dataResul);
 });
