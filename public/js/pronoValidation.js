107cb650dd2b07612eb3004351b18206a9ed0f75
victim0596 <81644467+victim0596@users.noreply.github.com>
2021-12-23 11:41:04 +0100

    Corretto cognome Russell

--- public/js/pronoValidation.js
+++ public/js/pronoValidation.js
@@ -1,5 +1,5 @@
 var driverArray = ["Hamilton", "Bottas", "Verstappen", "Perez", "Sainz", "Leclerc", "Ricciardo", "Norris", "Alonso", "Ocon",
-    "Giovinazzi", "Raikkonen", "Gasly", "Tsunoda", "Russel", "Latifi", "Mazepin", "Schumacher", "Vettel", "Stroll"];
+    "Giovinazzi", "Raikkonen", "Gasly", "Tsunoda", "Russell", "Latifi", "Mazepin", "Schumacher", "Vettel", "Stroll"];
 
 var pronoQualy = {
     qp1: "",
@@ -21,8 +21,6 @@ var gp1Form = document.getElementsByName("gp1")[0];
 var gp2Form = document.getElementsByName("gp2")[0];
 var gp3Form = document.getElementsByName("gp3")[0];
 var fastLapForm = document.getElementsByName("giro_veloce")[0];
-var textError = document.getElementsByClassName('text_pron')[0];
-var textErrorProfile = document.getElementsByClassName('text_modpron')[0];
 var btnQualy = document.getElementsByName("invia_quali")[0];
 var btnRace = document.getElementsByName("invia_race")[0];
 
@@ -71,19 +69,12 @@ fastLapForm.addEventListener('input', function () {
 //ricordati di cambiare i link
 function checkDriverQualy() {
     if (pronoQualy.qp1 == pronoQualy.qp2 || pronoQualy.qp1 == pronoQualy.qp3 || pronoQualy.qp2 == pronoQualy.qp3) {
-        if (urlPage == "http://localhost/Git/FantaGP/pronostici.php") textError.innerHTML = "Hai inserito uno stesso pilota in uno degli altri campi";
-        else textErrorProfile.innerHTML = "Hai inserito uno stesso pilota in uno degli altri campi";
-
     }
     else {
         if (driverArray.indexOf(pronoQualy.qp1) >= 0 && driverArray.indexOf(pronoQualy.qp2) >= 0 && driverArray.indexOf(pronoQualy.qp3) >= 0) {
             btnQualy.disabled = false;
-            if (urlPage == "http://localhost/Git/FantaGP/pronostici.php") textError.innerHTML = "";
-            else textErrorProfile.innerHTML = "";
         }
         else {
-            if (urlPage == "http://localhost/Git/FantaGP/pronostici.php") textError.innerHTML = "Hai inserito un pilota non esistente/Campo vuoto";
-            else textErrorProfile.innerHTML = "Hai inserito un pilota non esistente/Campo vuoto";
             btnQualy.disabled = true;
         }
     }
@@ -93,19 +84,13 @@ function checkDriverQualy() {
 //ricordati di cambiare i link
 function checkDriverRace() {
     if (pronoRace.gp1 == pronoRace.gp2 || pronoRace.gp1 == pronoRace.gp3 || pronoRace.gp2 == pronoRace.gp3) {
-        if (urlPage == "http://localhost/Git/FantaGP/pronostici.php") textError.innerHTML = "Hai inserito uno stesso pilota in uno degli altri campi";
-        else textErrorProfile.innerHTML = "Hai inserito uno stesso pilota in uno degli altri campi";
     }
     else {
         //controllo se i piloti del form gara sono esistenti
         if (driverArray.indexOf(pronoRace.gp1) >= 0 && driverArray.indexOf(pronoRace.gp2) >= 0 && driverArray.indexOf(pronoRace.gp3) >= 0 && driverArray.indexOf(pronoRace.fastLap) >= 0) {
             btnRace.disabled = false;
-            if (urlPage == "http://localhost/Git/FantaGP/pronostici.php") textError.innerHTML = "";
-            else textErrorProfile.innerHTML = "";
         }
         else {
-            if (urlPage == "http://localhost/Git/FantaGP/pronostici.php") textError.innerHTML = "Hai inserito un pilota non esistente/Campo vuoto";
-            else textErrorProfile.innerHTML = "Hai inserito un pilota non esistente/Campo vuoto";
             btnRace.disabled = true;
         }
     }
