c3a32d31f7e57812006aaaad946d31bf3a6eaa1d
victim0596 <81644467+victim0596@users.noreply.github.com>
2021-12-28 18:38:45 +0100

    cambiata UI alle classifiche

---
+++ public/js/classificaEstetica.js
@@ -0,0 +1,47 @@
+
+
+var allTdName = document.querySelectorAll("tr .nameDriver");
+
+var allImgScuderia = document.querySelectorAll("td .logoScuderia");
+
+var spanTd = document.querySelectorAll("tr .nameDriver span");
+
+
+
+
+
+var objDriver = {
+    alessiodom97: "alfa",
+    Oliver: "redbull",
+    SpiritoBlu: "Ferrari",
+    Toto: "haas",
+    Ciccio: "mercedes",
+    Andrea: "aston",
+    Dario: "mclaren",
+    pinguinoSquadraCorse: "williams",
+    Ermenegildo: "tauri",
+    gianpaolo: "Alpine"
+}
+
+var objColor = {
+    alessiodom97: "rgb(105, 13, 1)",
+    Oliver: "blue",
+    SpiritoBlu: "red",
+    Toto: "white",
+    Ciccio: "rgb(50, 168, 157)",
+    Andrea: "rgb(3, 130, 60)",
+    Dario: "orange",
+    pinguinoSquadraCorse: "white",
+    Ermenegildo: "rgb(8, 1, 59)",
+    gianpaolo: "blue"
+}
+
+
+for (var i = 0; i < allTdName.length; i++) {
+    var element = allTdName[i].innerText.replace(/\s+/, "").substring(1)
+    allImgScuderia[i].src = `img/scuderie/scuderieClassifica/${objDriver[element]}.png`;
+    spanTd[i].style.borderRightColor = `${objColor[element]}`;
+}
+
+
+
