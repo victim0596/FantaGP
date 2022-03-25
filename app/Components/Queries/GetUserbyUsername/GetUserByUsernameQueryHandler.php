24ae3aa0a0e9f947a06b7fbb7b055c6c59ced2f9
victim0596 <81644467+victim0596@users.noreply.github.com>
2022-03-25 09:06:47 +0100

    fix model nell'use

--- app/Components/Queries/GetUserbyUsername/GetUserByUsernameQueryHandler.php
+++ app/Components/Queries/GetUserbyUsername/GetUserByUsernameQueryHandler.php
@@ -2,7 +2,7 @@
 
 namespace App\Components\Queries\GetUserbyUsername;
 
-use App\Models\LoginModel;
+use App\Models\Utenti;
 
 
 class GetUserByUsernameQueryHandler  
@@ -10,7 +10,7 @@ class GetUserByUsernameQueryHandler
 
     public static function Retrieve(GetUserByUsernameQuery $query): GetUserByUsernameQueryResult
     {
-        $dbresult = LoginModel::where('USERNAME', $query->getUsername())->first();
+        $dbresult = Utenti::where('USERNAME', $query->getUsername())->first();
         $result = new GetUserByUsernameQueryResult($dbresult->USERNAME, $dbresult->PASSWORD, $dbresult->ADMIN);
         return $result;
     }
