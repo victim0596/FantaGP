edad81b91d5d6905412743c758a138eba6ccb5b0
victim0596 <81644467+victim0596@users.noreply.github.com>
2022-03-26 13:28:56 +0100

    Semplificata gestione classifica e char

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,6 +1,5 @@
-/* chart dario */
-var ctx_dario = document.getElementById('ChartDario').getContext('2d');
-var chart_dario = new Chart(ctx_dario, {
+var ctx = document.getElementById('chart').getContext('2d');
+var chart = new Chart(ctx, {
     type: 'line',
     data: {
         labels: [
@@ -20,215 +19,6 @@ var chart_dario = new Chart(ctx_dario, {
         }
     }
 });
-
-
-/* chart gianpaolo */
-var ctx_gianpaolo = document.getElementById('Chartgianpaolo').getContext('2d');
-var chart_gianpaolo = new Chart(ctx_gianpaolo, {
-    type: 'line',
-    data: {
-        labels: [
-            "Bahrein", "Arabia Saudita", "Australia", "Italia-Imola", "USA-Miami", "Spagna",
-            "Monaco", "Azerbaijan", "Canada", "Gran Bretagna", "Austria",
-            "Francia", "Ungheria", "Belgio", "Olanda", "Italia-Monza", "Singapore", "Giappone", "USA-Cota",
-            "Messico", "Brasile", "Emirati Arabi"
-        ],
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-/* chart oliver */
-var ctx_oliver = document.getElementById('ChartOliver').getContext('2d');
-var chart_oliver = new Chart(ctx_oliver, {
-    type: 'line',
-    data: {
-        labels: [
-            "Bahrein", "Arabia Saudita", "Australia", "Italia-Imola", "USA-Miami", "Spagna",
-            "Monaco", "Azerbaijan", "Canada", "Gran Bretagna", "Austria",
-            "Francia", "Ungheria", "Belgio", "Olanda", "Italia-Monza", "Singapore", "Giappone", "USA-Cota",
-            "Messico", "Brasile", "Emirati Arabi"
-        ],
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-/* chart alessioc */
-var ctx_alessioc = document.getElementById('ChartErmenegildo').getContext('2d');
-var chart_alessioc = new Chart(ctx_alessioc, {
-    type: 'line',
-    data: {
-        labels: [
-            "Bahrein", "Arabia Saudita", "Australia", "Italia-Imola", "USA-Miami", "Spagna",
-            "Monaco", "Azerbaijan", "Canada", "Gran Bretagna", "Austria",
-            "Francia", "Ungheria", "Belgio", "Olanda", "Italia-Monza", "Singapore", "Giappone", "USA-Cota",
-            "Messico", "Brasile", "Emirati Arabi"
-        ],
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-/* chart alessiod */
-var ctx_alessiod = document.getElementById('Chartalessiodom97').getContext('2d');
-var chart_alessiod = new Chart(ctx_alessiod, {
-    type: 'line',
-    data: {
-        labels: [
-            "Bahrein", "Arabia Saudita", "Australia", "Italia-Imola", "USA-Miami", "Spagna",
-            "Monaco", "Azerbaijan", "Canada", "Gran Bretagna", "Austria",
-            "Francia", "Ungheria", "Belgio", "Olanda", "Italia-Monza", "Singapore", "Giappone", "USA-Cota",
-            "Messico", "Brasile", "Emirati Arabi"
-        ],
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-/* chart pino */
-var ctx_pino = document.getElementById('ChartpinguinoSquadraCorse').getContext('2d');
-var chart_pino = new Chart(ctx_pino, {
-    type: 'line',
-    data: {
-        labels: [
-            "Bahrein", "Arabia Saudita", "Australia", "Italia-Imola", "USA-Miami", "Spagna",
-            "Monaco", "Azerbaijan", "Canada", "Gran Bretagna", "Austria",
-            "Francia", "Ungheria", "Belgio", "Olanda", "Italia-Monza", "Singapore", "Giappone", "USA-Cota",
-            "Messico", "Brasile", "Emirati Arabi"
-        ],
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-/* chart andrea */
-var ctx_andrea = document.getElementById('ChartAndrea').getContext('2d');
-var chart_andrea = new Chart(ctx_andrea, {
-    type: 'line',
-    data: {
-        labels: [
-            "Bahrein", "Arabia Saudita", "Australia", "Italia-Imola", "USA-Miami", "Spagna",
-            "Monaco", "Azerbaijan", "Canada", "Gran Bretagna", "Austria",
-            "Francia", "Ungheria", "Belgio", "Olanda", "Italia-Monza", "Singapore", "Giappone", "USA-Cota",
-            "Messico", "Brasile", "Emirati Arabi"
-        ],
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-/* chart toto */
-var ctx_toto = document.getElementById('ChartToto').getContext('2d');
-var chart_toto = new Chart(ctx_toto, {
-    type: 'line',
-    data: {
-        labels: [
-            "Bahrein", "Arabia Saudita", "Australia", "Italia-Imola", "USA-Miami", "Spagna",
-            "Monaco", "Azerbaijan", "Canada", "Gran Bretagna", "Austria",
-            "Francia", "Ungheria", "Belgio", "Olanda", "Italia-Monza", "Singapore", "Giappone", "USA-Cota",
-            "Messico", "Brasile", "Emirati Arabi"
-        ],
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-/* chart luca */
-var ctx_luca = document.getElementById('ChartSpiritoBlu').getContext('2d');
-var chart_luca = new Chart(ctx_luca, {
-    type: 'line',
-    data: {
-        labels: [
-            "Bahrein", "Arabia Saudita", "Australia", "Italia-Imola", "USA-Miami", "Spagna",
-            "Monaco", "Azerbaijan", "Canada", "Gran Bretagna", "Austria",
-            "Francia", "Ungheria", "Belgio", "Olanda", "Italia-Monza", "Singapore", "Giappone", "USA-Cota",
-            "Messico", "Brasile", "Emirati Arabi"
-        ],
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-/* chart ciccio */
-var ctx_ciccio = document.getElementById('ChartCiccio').getContext('2d');
-var chart_ciccio = new Chart(ctx_ciccio, {
-    type: 'line',
-    data: {
-        labels: [
-            "Bahrein", "Arabia Saudita", "Australia", "Italia-Imola", "USA-Miami", "Spagna",
-            "Monaco", "Azerbaijan", "Canada", "Gran Bretagna", "Austria",
-            "Francia", "Ungheria", "Belgio", "Olanda", "Italia-Monza", "Singapore", "Giappone", "USA-Cota",
-            "Messico", "Brasile", "Emirati Arabi"
-        ],
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
 
 
 function addData(chart, data) {
@@ -254,6 +44,70 @@ function addData(chart, data) {
 function updateData(chart, data) {
     chart.data.datasets[0].data = data;
     chart.update();
+}
+
+var btnChart = document.querySelectorAll('tr td button');
+
+btnChart.forEach(x => {
+    x.addEventListener('click', () => {
+        var trElement = x.parentElement.parentElement;
+        var nameChild = trElement.childNodes[3].innerText.substring(1);
+        //imposto il titolo del grafico
+        document.getElementById('modalchartTitle').innerHTML = `Andamento punti per gara ${nameChild}`;
+        fetch(`./api/pronoAllClassifica/${nameChild}`)
+            .then(
+                function (response) {
+                    if (response.status !== 200) {
+                        console.log('Looks like there was a problem. Status Code: ' +
+                            response.status);
+                        return;
+                    }
+                    // Examine the text in the response
+                    response.json().then(function (data) {
+                        setDataChart(data);
+                    });
+                }
+            )
+            .catch(function (err) {
+                console.log('Fetch Error :-S', err);
+            });
+    })
+})
+
+function setDataChart(chartObjData) {
+    var tipoClassifica = document.getElementById('modalTitoloClassifica').innerText.substring(11);
+    switch (tipoClassifica) {
+        case 'Generale':
+            var values = chartObjData.map(function (e) {
+                return e.punti;
+            });
+            if (chart.data.datasets[0]) {
+                updateData(chart, values)
+            } else {
+                addData(chart, values)
+            }
+            break;
+        case 'Pagelle':
+            var values = chartObjData.map(function (e) {
+                return e.punti_pag;
+            });
+            if (chart.data.datasets[0]) {
+                updateData(chart, values)
+            } else {
+                addData(chart, values)
+            }
+            break;
+        case 'Pronostici':
+            var values = chartObjData.map(function (e) {
+                return e.punti_pron;
+            });
+            if (chart.data.datasets[0]) {
+                updateData(chart, values)
+            } else {
+                addData(chart, values)
+            }
+            break;
+    }
 }
 
 
--- public/js/classificaEstetica.js
+++ public/js/classificaEstetica.js
@@ -1,47 +1,45 @@
+function setClassificaEstetica() {
+    var allTdName = document.querySelectorAll("tr .nameDriver");
+
+    var allImgScuderia = document.querySelectorAll("td .logoScuderia");
+
+    var spanTd = document.querySelectorAll("tr .nameDriver span");
+
+    var objDriver = {
+        alessiodom97: "alfa",
+        Oliver: "redbull",
+        SpiritoBlu: "Ferrari",
+        Toto: "haas",
+        Ciccio: "mercedes",
+        Andrea: "aston",
+        Dario: "mclaren",
+        pinguinoSquadracorse: "williams",
+        Ermenegildo: "tauri",
+        gianpaolo: "Alpine"
+    }
+
+    var objColor = {
+        alessiodom97: "rgb(105, 13, 1)",
+        Oliver: "blue",
+        SpiritoBlu: "red",
+        Toto: "white",
+        Ciccio: "rgb(50, 168, 157)",
+        Andrea: "rgb(3, 130, 60)",
+        Dario: "orange",
+        pinguinoSquadracorse: "white",
+        Ermenegildo: "rgb(8, 1, 59)",
+        gianpaolo: "rgba(0, 119, 255, 0.836"
+    }
 
 
-var allTdName = document.querySelectorAll("tr .nameDriver");
-
-var allImgScuderia = document.querySelectorAll("td .logoScuderia");
-
-var spanTd = document.querySelectorAll("tr .nameDriver span");
-
-
-
-
-
-var objDriver = {
-    alessiodom97: "alfa",
-    Oliver: "redbull",
-    SpiritoBlu: "Ferrari",
-    Toto: "haas",
-    Ciccio: "mercedes",
-    Andrea: "aston",
-    Dario: "mclaren",
-    pinguinoSquadracorse: "williams",
-    Ermenegildo: "tauri",
-    gianpaolo: "Alpine"
+    for (var i = 0; i < allTdName.length; i++) {
+        var element = allTdName[i].innerText.replace(/\s+/, "").substring(1)
+        allImgScuderia[i].src = `img/scuderie/scuderieClassifica/${objDriver[element]}.png`;
+        spanTd[i].style.borderRightColor = `${objColor[element]}`;
+    }
 }
 
-var objColor = {
-    alessiodom97: "rgb(105, 13, 1)",
-    Oliver: "blue",
-    SpiritoBlu: "red",
-    Toto: "white",
-    Ciccio: "rgb(50, 168, 157)",
-    Andrea: "rgb(3, 130, 60)",
-    Dario: "orange",
-    pinguinoSquadracorse: "white",
-    Ermenegildo: "rgb(8, 1, 59)",
-    gianpaolo: "rgba(0, 119, 255, 0.836"
-}
 
-
-for (var i = 0; i < allTdName.length; i++) {
-    var element = allTdName[i].innerText.replace(/\s+/, "").substring(1)
-    allImgScuderia[i].src = `img/scuderie/scuderieClassifica/${objDriver[element]}.png`;
-    spanTd[i].style.borderRightColor = `${objColor[element]}`;
-}
 
 
 
